58e3380d63f271119f20d0ec8a4701dc21508e46
Sérvulo Sávio Medeiros <38638324+servulosavio@users.noreply.github.com>
2022-08-19 20:28:28 -0300

    Questão 4 - Em desenvolvimento

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// var num = prompt('Digite um número');
+// undefined
+// if (num %2 == 0) {
+//     console.log('O número ' + num + ' é Par')
+// } else {
+//     console.log('O número ' + num + ' é Ímpar')
+// }
+
+
 var num = prompt('Digite um número');
-undefined
-if (num %2 == 0) {
-    console.log('O número ' + num + ' é Par')
-} else {
-    console.log('O número ' + num + ' é Ímpar')
+var a = [];
+
+for (let i = 0; i < num; i++) {
+    a.push('*')
+    console.log(a)
 }
+
